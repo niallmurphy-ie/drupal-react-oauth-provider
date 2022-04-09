ec0bd8c148243c215a3db9b2dbc09c1eca733ed4
Niall Murphy <leturf@gmail.com>
2022-04-09 16:29:55 +0700

    isAuthenticated removed from index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export * from './hooks/useAPI';
 export * from './hooks/useLazyAPI';
 export * from './hooks/user/useLazyLogin';
-export * from './hooks/user/isAuthenticated';
 export * from './hooks/headers/useHeaders';
 export * from './hooks/user/useLazyLogout';
 export * from './hooks/user/useAuthenticated';
